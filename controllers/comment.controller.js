1c2603c55c3a26bf56a49ad150b85bbda6434ef5
Isa Gutierrez <isagr_kio@hotmail.com>
2022-02-27 15:46:05 +0100

    Added html, css and model for Comment.

---
+++ controllers/comment.controller.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Comment = require('../models/comment.model');
+const User =  require('../models/user.model');
+const Book = require('../models/book.model');
+const Room = require('../models/room.model');
+
+// Create a new comment
+
+// Edit comment
+
+// Delete comment
