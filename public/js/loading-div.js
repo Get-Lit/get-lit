de88a06910b3744b27f98a2bacead9c6bd88140e
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-03-08 13:37:05 +0100

    fix loading-page script bug

--- public/js/loading-div.js
+++ public/js/loading-div.js
@@ -1,6 +1,8 @@
 window.onload = () => {
-    setTimeout(() => {
-        document.querySelector('.loading-div').style.display = 'none';
-        document.querySelector('.loading-img').style.display = 'none';
-    }, 3000);
+    if(document.querySelector('.loading-div') && document.querySelector('.loading-img')) {
+        setTimeout(() => {
+            document.querySelector('.loading-div').style.display = 'none';
+            document.querySelector('.loading-img').style.display = 'none';
+        }, 3000);
+    }
 }
